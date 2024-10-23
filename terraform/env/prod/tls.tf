40586d6b2d01949fa3bcaf147226c687fa0703b2
Trevor Johnson <tjohnson1293@gmail.com>
2024-10-23 13:40:30 -0700

    updated provider definitions to pull latest aws provider and cleanup outdated provider config

--- terraform/env/prod/tls.tf
+++ terraform/env/prod/tls.tf
@@ -4,11 +4,6 @@
 
 module "webcert" {
   source = "../../modules/acm"
-
-  providers = {
-    aws.acm = aws,
-    aws.dns = aws
-  }
   domain_name = var.registered_domain
   zone_id     = data.aws_route53_zone.primary.id
 
@@ -19,11 +14,6 @@ module "webcert" {
 
 module "apicert" {
   source = "../../modules/acm"
-
-  providers = {
-    aws.acm = aws,
-    aws.dns = aws
-  }
   domain_name = var.api_subdomain
   zone_id     = data.aws_route53_zone.primary.id
 
--- terraform/modules/acm/required-providers.tf
+++ terraform/modules/acm/required-providers.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 0.13.1"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 5.0, < 6.0"
     }
   }
 }
